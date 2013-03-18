d134d6d9b18bea940448d0fcb47847e7c8e17304
victor <victor@NRSI-Gaultney-Precise>
2013-03-18 17:05:15 +0000

    Adding remove overlap script

---
+++ fonts/nokyung/font-source/FFRemoveOverlapAll.py
@@ -0,0 +1,13 @@
+# FFRemoveOverlapAll.py
+# FontForge: Remove overlap on all glyphs in font
+# VG 22 Feb 2012
+# usage: python FFRemoveOverlapAll.py [sourcefont.sfd] [resultfont.sfd]
+
+import sys, fontforge
+
+source = fontforge.open(sys.argv[1])
+result = sys.argv[2]
+
+for glyph in source:
+	source[glyph].removeOverlap()
+source.save(result)
